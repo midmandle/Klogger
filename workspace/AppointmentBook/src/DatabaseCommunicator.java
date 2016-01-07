1b9c2a8aecb0b11521805180c74f614c1354b85a
Midmandle <alasdairsmith0491@gmail.com>
2016-01-07 12:21:54 +0000

    Continuous development tot he DatabaseCommunicator.java

--- workspace/AppointmentBook/src/DatabaseCommunicator.java
+++ workspace/AppointmentBook/src/DatabaseCommunicator.java
@@ -1,9 +1,10 @@
 import java.sql.*;
 
 public class DatabaseCommunicator {
-	Connection dbConnection = null;
-	public DatabaseCommunicator()
+	
+	private static Connection getDatabaseCommunicator()
 	{
+		Connection dbConnection = null;
 		try
 		{
 			Class.forName("org.sqlite.JDBC");
@@ -15,14 +16,48 @@ public class DatabaseCommunicator {
 		
 		try
 		{
-			String dbURL = "INSERT_PROPER_URL_HERE"; //TODO: Insert URL (jdbc:sqlite:<somename>.sqlite)
+			String dbURL = "jdbc:sqlite:main.sqlite"; //TODO: Insert URL (jdbc:sqlite:<somename>.sqlite)
 			dbConnection = DriverManager.getConnection(dbURL);
+			return dbConnection;
 		}
 		catch( SQLException e)
 		{
 			System.out.println(e.getMessage());
 		}
+		return dbConnection;
 	}
 	
 	//TODO: SQL: Make request.
+	public static ResultSet MakeRequest(String query) throws SQLException
+	{
+		Connection dbConnection = null;
+		Statement statement = null;
+		ResultSet resultSet = null;
+		//String query = "CREATE TABLE testTable(a INTEGER, b INTEGER, PRIMARY KEY (a ASC));";
+		
+		try
+		{
+			dbConnection = getDatabaseCommunicator();
+			statement = dbConnection.createStatement();
+			System.out.println(query);
+			resultSet = statement.executeQuery(query);
+			return resultSet;
+		}
+		catch (SQLException e)
+		{
+			System.out.println(e.getMessage());
+		}
+		finally
+		{
+			
+			if(resultSet != null)
+				resultSet.close();
+			if(resultSet != null)
+				statement.close();
+			if(dbConnection != null)
+				dbConnection.close();
+			
+		}
+		return resultSet;
+	}
 }
--- workspace/AppointmentBook/src/Main.java
+++ workspace/AppointmentBook/src/Main.java
@@ -1,4 +1,6 @@
 import java.awt.FlowLayout;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.swing.JFrame;
 
@@ -13,6 +15,21 @@ public class Main {
 				f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				f.setSize(600, 400);
 				f.setVisible(true);
+				
+				//DatabaseCommunicator dbComms = new DatabaseCommunicator();
+				try {
+					ResultSet res = null;
+					//res = DatabaseCommunicator.MakeRequest("INSERT INTO testTable (a, b) VALUES (80, 4);");
+					res = DatabaseCommunicator.MakeRequest("SELECT * FROM testTable;");
+					while(res.next())//TODO: BUG: At this point res seems to still be null
+					{
+						System.out.println("LOL"); //Never executes...
+						System.out.println(res.absolute(0));
+					}
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		});
 		
