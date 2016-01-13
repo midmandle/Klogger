c8f00e1d9a980410c5f330faf334258eed7fd1c4
Midmandle <alasdairsmith0491@gmail.com>
2016-01-13 21:48:27 +0000

    Massive upgrade to the way DatabaseCommunicator class handles requests generically.

--- workspace/AppointmentBook/src/DatabaseCommunicator.java
+++ workspace/AppointmentBook/src/DatabaseCommunicator.java
@@ -1,117 +1,229 @@
 import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class DatabaseCommunicator {
-	private Connection dbConnection = null;
+	private static Connection dbConnection = null;
+	private static Statement statement = null;
+	private static ResultSet resultSet = null;
 	
-	public DatabaseCommunicator()
-	{
-		setupComms();
-	}
-	
-	private void setupComms()
+	private static void MakeRequest(String query)
 	{
 		try
 		{
-			Class.forName("org.sqlite.JDBC");
+			try
+			{
+				Class.forName("org.sqlite.JDBC");
+				String dbURL = "jdbc:sqlite:main.sqlite"; //Insert DB URL (jdbc:sqlite:<somename>.sqlite)
+				dbConnection = DriverManager.getConnection(dbURL);
+				statement = dbConnection.createStatement();
+				statement.execute(query);
+			}
+			finally
+			{
+				if(statement != null)
+					statement.close();
+				if(dbConnection != null)
+					dbConnection.close();
+			}
 		}
-		catch (ClassNotFoundException e)
+		catch(SQLException | ClassNotFoundException e)
 		{
 			System.out.println(e.getMessage());
 		}
-		
+	}
+	
+	@SuppressWarnings("unchecked")
+	private static void MakeRequest(String query, ArrayList<ArrayList<?>> output)
+	{
 		try
 		{
-			String dbURL = "jdbc:sqlite:main.sqlite"; //Insert DB URL (jdbc:sqlite:<somename>.sqlite)
-			dbConnection = DriverManager.getConnection(dbURL);
-			
+			try
+			{
+				Class.forName("org.sqlite.JDBC");
+				String dbURL = "jdbc:sqlite:main.sqlite"; //Insert DB URL (jdbc:sqlite:<somename>.sqlite)
+				dbConnection = DriverManager.getConnection(dbURL);
+				statement = dbConnection.createStatement();
+				
+				resultSet = statement.executeQuery(query);
+				ResultSetMetaData resMet = resultSet.getMetaData();
+				int numberOfColumns = resMet.getColumnCount();
+				
+				for(int i = 1; i <= numberOfColumns; i++)
+				{
+					int t = resMet.getColumnType(i);
+					
+					switch(t)
+					{
+						case Types.VARCHAR:
+						{
+							if(resMet.getColumnName(i).startsWith("date"))
+							{
+								output.add(new ArrayList<GregorianCalendar>());
+							}
+							else
+								output.add(new ArrayList<String>());
+						}
+						default:
+							output.add(new ArrayList<Object>());
+					}
+				}
+				
+				while(resultSet.next())
+				{
+					for(int i = 1; i <= numberOfColumns; i++) 
+					{
+						int t = resMet.getColumnType(i);
+						
+						switch(t)
+						{
+							case Types.VARCHAR:
+							{
+								if(resMet.getColumnName(i).startsWith("date")) //Its a date.
+								{
+									//retrieve dateTime string and parse to GregorianCal.
+									GregorianCalendar calDateTimeFromDB = new GregorianCalendar();
+									FormattedDateToGregorian(resultSet.getString(i), calDateTimeFromDB);
+									
+									ArrayList<GregorianCalendar> tempCalList = (ArrayList<GregorianCalendar>) output.get(i);
+									tempCalList.add(calDateTimeFromDB);
+									output.set(i, tempCalList);//ERROR
+								}
+								else //Its a regular varchar
+								{
+									ArrayList<String> tempStringList = (ArrayList<String>) output.get(i);
+									tempStringList.add(resultSet.getString(i));
+									output.set(i, tempStringList); //ERROR
+								}
+								break;
+							}
+							default:
+							{
+								//System.out.println("Unexpected type.");
+								break;
+							}
+						}
+					}
+				}
+				
+			}
+			finally
+			{
+				if(resultSet != null)
+					resultSet.close();
+				if(statement != null)
+					statement.close();
+				if(dbConnection != null)
+					dbConnection.close();
+			}
 		}
-		catch( SQLException e)
+		catch(SQLException | ClassNotFoundException e)
 		{
 			System.out.println(e.getMessage());
 		}
 	}
 	
-	private void MakeRequest(String query) throws SQLException
+	private static String dateFormatter(GregorianCalendar cal)
 	{
-		Statement statement = dbConnection.createStatement();
-		statement.execute(query);
-		
-		statement.close();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		sdf.setCalendar(cal);
+		String output = sdf.format(cal.getTime());
+		//System.out.println(output);
+		return output;
 	}
 	
-	public void AddAppointmentToDatabase(String tableName, Appointment appointment)
+	private static void FormattedDateToGregorian(String formatted, GregorianCalendar output)
 	{
-		int startDay = appointment.getStartDateTime().get(Calendar.DAY_OF_MONTH);
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		
+		try {
+			java.util.Date dateTime = sdf.parse(formatted);
+			output.setTime(dateTime);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void AddAppointmentToDatabase(String tableName, Appointment appointment)
+	{
+		/*int startDay = appointment.getStartDateTime().get(Calendar.DAY_OF_MONTH);
 		int startMonth = appointment.getStartDateTime().get(Calendar.MONTH);
 		int startYear = appointment.getStartDateTime().get(Calendar.YEAR);
-		int endDay = appointment.getEndDateTime().get(Calendar.DAY_OF_MONTH);
+		int endDay = appointment.getEndDateTime().get(Calendar.DAY_OF_MONTH);			//DEPRECATED
 		int endMonth = appointment.getEndDateTime().get(Calendar.MONTH);
-		int endYear = appointment.getEndDateTime().get(Calendar.YEAR);
+		int endYear = appointment.getEndDateTime().get(Calendar.YEAR);*/
+		
+		String startDate = dateFormatter(appointment.getStartDateTime());
+		String endDate = dateFormatter(appointment.getEndDateTime());
 		
 		String eventTitle = appointment.getEventTitle();
 		String description = appointment.getEventTitle();
 		String location = "null";
-		String query = String.format("INSERT INTO "+tableName+" (dateTimeFrom, dateTimeTo, eventTitle, description, location) VALUES (\"%d/%d/%d\", \"%d/%d/%d\", \"%s\", \"%s\", \"%s\");", startDay, startMonth, startYear, endDay, endMonth, endYear, eventTitle, description, location);
+		String query = String.format("INSERT INTO "+tableName+" (dateTimeFrom, dateTimeTo, eventTitle, description, location) VALUES (\"%s\", \"%s\", \"%s\", \"%s\", \"%s\");", startDate, endDate, eventTitle, null, location);
 		
-		try {
-			MakeRequest(query);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		MakeRequest(query);
+		
 	}
 	
-	public void RemoveAppointmentFromDatabase(String tableName, Appointment appointment)
+	public static void RemoveAppointmentFromDatabase(String tableName, Appointment appointment)
 	{
 		String query = "DELETE FROM Book1 WHERE title == \""+appointment.getEventTitle()+"\";";
 		
-		try {
-			MakeRequest(query);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		MakeRequest(query);
+		
 	}
 	
-	public void GetAllAppointmentsFromDatabase(String tableName, Appointment appointment)
+	public static void GetAllAppointmentsFromDatabase(String tableName)//, Appointment appointment)
 	{
-		//Returns result.
+		String query = "SELECT * FROM "+tableName+";";
+		ArrayList<ArrayList<?>> results = new ArrayList<ArrayList<?>>();
+		MakeRequest(query, results); //TODO: Play with results list. Nested list thing.
+		
+		for(int i = 0 ; i < results.size(); i++)
+			for(int j = 0; j < results.get(i).size(); j++)
+			{
+				if(results.get(i).get(j) instanceof GregorianCalendar)
+				{
+					GregorianCalendar date = (GregorianCalendar) results.get(i).get(j);
+					System.out.println(date.get(Calendar.YEAR));
+				}
+					
+			}
+	
 	}
 	
-	public void GetSecificAppointmentFromDatabase(String tableName, Appointment appointment)
+	public static void GetSecificAppointmentFromDatabase(String tableName, Appointment appointment)
 	{
 		//Returns results;
 	}
 	
-	public boolean CheckIfAppointmentBookExistsOnDatabase(String tableName)
+	public static boolean CheckIfAppointmentBookExistsOnDatabase(String tableName)
 	{
+		String query = "SELECT * FROM sqlite_master;";
+		ArrayList<ArrayList<?>> results = new ArrayList<ArrayList<?>>();
+		MakeRequest(query, results);
 		
 		return false;
 	}
 	
-	public void SetupNewAppointmentBookForDatabase(String tableName)
+	public static void SetupNewAppointmentBookForDatabase(String tableName)
 	{
-		String query = "CREATE TABLE IF NOT EXISTS "+ tableName +"(dateTimeFrom DATE NOT NULL, dateTimeTo DATE, eventTitle VARCHAR NOT NULL, description VARCHAR, location VARCHAR);";
+		String query = "CREATE TABLE IF NOT EXISTS "+ tableName +"(dateTimeFrom TEXT NOT NULL, dateTimeTo TEXT, eventTitle VARCHAR NOT NULL, description VARCHAR, location VARCHAR);";
 		
-		try {
-			MakeRequest(query);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		MakeRequest(query);
+		
 	}
 	
-	public void RemoveAppointmentBookFromDatabase(String tableName)
+	public static void RemoveAppointmentBookFromDatabase(String tableName)
 	{
 		String query = "DROP TABLE \"main\".\""+tableName+"\";";
 		
-		try {
-			MakeRequest(query);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		MakeRequest(query);
+		
 	}
 
 	
