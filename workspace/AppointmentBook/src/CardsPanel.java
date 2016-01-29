68817c6d22d6e7d8accc388fa1d5cb5f9a67e95c
Midmandle <alasdairsmith0491@gmail.com>
2016-01-29 15:13:27 +0000

    Updated the UI.

---
+++ workspace/AppointmentBook/src/CardsPanel.java
@@ -0,0 +1,19 @@
+import java.awt.CardLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+
+
+public class CardsPanel extends JPanel{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public CardsPanel()
+	{
+		
+	}
+}
